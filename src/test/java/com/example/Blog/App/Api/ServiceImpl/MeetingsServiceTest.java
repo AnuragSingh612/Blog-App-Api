acd998a8f7fc66fec5585fa0331386d6c6035279
Dell <anurag.singh2018@gmail.com>
2023-04-25 11:45:56 +0530

    meetingtype and updated on fixed!

--- src/test/java/com/example/Blog/App/Api/ServiceImpl/MeetingsServiceTest.java
+++ src/test/java/com/example/Blog/App/Api/ServiceImpl/MeetingsServiceTest.java
@@ -1,72 +1,82 @@
 package com.example.Blog.App.Api.ServiceImpl;
 
-import com.example.Blog.App.Api.payload.SprintDto;
+
+import com.example.Blog.App.Api.Entity.MeetingPlatforms;
+import com.example.Blog.App.Api.Entity.Meetings;
+import com.example.Blog.App.Api.Entity.Sprints;
 import com.example.Blog.App.Api.payload.meetingDto;
+import com.example.Blog.App.Api.repository.MeetingPlatformRepo;
 import com.example.Blog.App.Api.repository.MeetingsRepo;
 import com.example.Blog.App.Api.repository.SprintRepo;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-@ExtendWith(MockitoExtension.class)
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
 class MeetingsServiceTest {
-    @Mock
-    MeetingsRepo meetingsRepo;
-    @Mock
-    SprintRepo sprintRepo;
-    MeetingsService meetingsService;
-    meetingDto m= new meetingDto();
-    SprintDto sprintDto= new SprintDto();
-    SprintService sprintService;
-    @Test
-    void createAmeeting() {
-        sprintDto.setCreatedOn(LocalDateTime.now());
-        sprintDto.setStartDate(LocalDateTime.now());
-        sprintDto.setSprintName("Sprint002");
-        sprintDto.setProjectCode("10001");
-        sprintDto.setSprintId(2);
-        sprintDto.setEndDate(LocalDateTime.now());
-        m.setId(1);
-        m.setCreatedOn(LocalDateTime.now());
-        m.setMeeting_platform_id(1);
-        m.setMeetingLink("testing");
-        m.setMeetingTime(LocalDateTime.now());
-        m.setMeetingPassword("password");
-        m.setMeetingDate(LocalDateTime.now());
-        m.setStatus("scheduled");
-        m.setSprintId(2);
-        m.setMeetingType("SprintReview");
-        m.setUpdatedOn(LocalDateTime.now());
-        meetingsService.createAmeeting(m);
-        verify(meetingsRepo).findById(1);
+//
+//    SprintRepo sprintRepo = Mockito.mock(SprintRepo.class);
+//
+//    @Mock
+//    MeetingPlatformRepo meetingPlatformsRepo;
+//
+//    @Mock
+//    MeetingsRepo meetingsRepo;
+//
+//    @Mock
+//    ModelMapper modelMapper;
+//
+//    @InjectMocks
+//    MeetingsService meetingsService;
+//
+//
+//
+//    @Test
+//    void createAMeeting() {
+//        meetingDto meetingDtoDailyScrum = new meetingDto();
+//        meetingDtoDailyScrum.setMeetingLink("meetingLink");
+//        meetingDtoDailyScrum.setMeetingPassword("meetingPassword");
+//        meetingDtoDailyScrum.setMeetingType("DailyScrum"); // it is for Daily Scrum
+//        meetingDtoDailyScrum.setMeetingTime(LocalTime.now());
+//        meetingDtoDailyScrum.setStatus("status");
+//
+//        Sprints responseSprint = new Sprints();
+//        responseSprint.setStartDate(LocalDateTime.now());
+//        responseSprint.setEndDate(LocalDateTime.now());
+//
+//        MeetingPlatforms responseMeetingPlatforms = new MeetingPlatforms();
+//
+//        List<Meetings> meetingsList = new ArrayList<>();
+//
+//        Meetings meeting = new Meetings();
+//
+//        when(sprintRepo.findById(anyInt())).thenReturn(Optional.of(responseSprint));
+//        when(meetingPlatformsRepo.findById(anyInt())).thenReturn(Optional.of(responseMeetingPlatforms));
+//        when(meetingsRepo.saveAll(anyList())).thenReturn(meetingsList);
+//        when(meetingsRepo.save(any(Meetings.class))).thenReturn(meeting);
+//        when(modelMapper.map(any(meetingDto.class),any())).thenReturn(meeting);
+//
+//
+//        meetingsService.createAmeeting(meetingDtoDailyScrum);
+////        meetingDtoDailyScrum.setMeetingType("SprintPlanning");
+////        verify(meetingsService).createAmeeting(meetingDtoDailyScrum);
+////        meetingDtoDailyScrum.setMeetingType("SprintRetrospective");
+////        verify(meetingsService).createAmeeting(meetingDtoDailyScrum);
+////        meetingDtoDailyScrum.setMeetingType("Test");
+////        verify(meetingsService).createAmeeting(meetingDtoDailyScrum);
+//        verify(sprintRepo).findById(1);
+//        verify(meetingPlatformsRepo).findById(1);
+//        verify(meetingsRepo).saveAll(meetingsList);
 
-    }
-
-    @Test
-    void getMetingbyId() {
-    }
-
-    @BeforeEach
-    void setUp() {
-        this.meetingsService= new MeetingsService(this.meetingsRepo);
-        this.sprintService= new SprintService(this.sprintRepo);
-    }
-
-    @Test
-    void reschedule() {
-    }
-
-    @Test
-    void getmeetingbyid() {
-    }
-
-    @Test
-    void updatepost() {
-    }
 }
